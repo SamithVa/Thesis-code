fb7e6ee2fa2bd2aa71b2d271d3659557d09e3846
wanshan <vasamith12@gmail.com>
2025-04-26 00:33:37 +0800

    add showui in test prefill_time script

--- prefill_tflop_estimation.py
+++ prefill_tflop_estimation.py
@@ -11,11 +11,13 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--showui", action="store_true"
+    )
+
+    parser.add_argument(
         '--sim', action="store_true", help="Implement similarity based on model"
     )
     args = parser.parse_args()
-    if args.uimask and args.sim:
-        parser.error("Arguments --uimask and --sim cannot both be true at the same time.")
 
     messages = [
             {
@@ -112,21 +114,11 @@ if __name__ == "__main__":
             )
             inputs = inputs.to(device)
             visual_tokens = inputs['pixel_values'].shape[0] // 4
-            if args.uimask:
-                dropped_visual_tokens = inputs['input_ids'].shape[1] - inputs['select_mask'].sum()
-                # input_ids shape [bsz, seq_len, dim], seq_len = text + visual 
-                # select_mask : [bsz, seq_len], select_mask.sum() = text + reduced_visual 
-                # -> input_ids.shape[1] - select_mask.sum() = # of dropped visual tokens 
-            else:
-                dropped_visual_tokens = 0
-            
-            # IMAGE_TOKEN_ID = 151655
-            # number_visual_tokens = (inputs['input_ids'] == IMAGE_TOKEN_ID).sum()
-            # print(number_visual_tokens)
-            
+            dropped_visual_tokens = inputs['input_ids'].shape[1] - inputs['select_mask'].sum()
+            # input_ids shape [bsz, seq_len, dim], seq_len = text + visual 
+            # select_mask : [bsz, seq_len], select_mask.sum() = text + reduced_visual 
+            # -> input_ids.shape[1] - select_mask.sum() = # of dropped visual tokens 
             print("Number of Visual Tokens :", visual_tokens - dropped_visual_tokens)
-            
-
             # Timed inference
             start_event = torch.cuda.Event(enable_timing=True)
             end_event = torch.cuda.Event(enable_timing=True)
@@ -223,6 +215,97 @@ if __name__ == "__main__":
             del inputs
             del generated_ids
             torch.cuda.empty_cache()
+
+    elif args.showui:
+        print("Tesing SHOWUI Inference Speed ...")
+        from ShowUI.showui import ShowUIForConditionalGeneration, ShowUIProcessor
+        
+        # 1. Screenshot -> Graph
+        uigraph_train = True  # Enable ui graph during training
+        uigraph_test = True  # Enable ui graph during inference
+        uigraph_diff = 1  # Pixel difference used for constructing ui graph
+        uigraph_rand = False  # Enable random graph construction
+        # 2. Graph -> Mask
+        uimask_pre = True  # Prebuild patch selection mask in the preprocessor (not in model layers) for efficiency
+        uimask_ratio = 1 # Specify the percentage of patch tokens to skip per component
+        uimask_rand = False  # Enable random token selection instead of uniform selection
+        
+
+        lm_qwen_layer = 28 # LLM Decoder layers
+        def parse_layer_type(str_ranges, L=lm_qwen_layer, default=0):
+            # 0 is without layer token selection, 1 is with layer token selection. Below we provide examples:
+            # [1,28,1] means that all LM layers use token selection; [1,28,0] means that do not.
+            # Interleaved layer-wise '[2,2,1],[4,4,1],[6,6,1],[8,8,1],[10,10,1],[12,12,1],[14,14,1],[16,16,1],[18,18,1],[20,20,1],[22,22,1],[24,24,1],[26,26,1]'
+            result = [default] * L
+            matches = re.findall(r'\[\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\]', str_ranges)
+            for start, end, value in matches:
+                start, end, value = int(start) - 1, int(end) - 1, int(value)
+                if end >= L:
+                    end = L - 1
+                result[start:end + 1] = [value] * (end - start + 1)
+            return result
+
+        lm_skip_layer = "[1,28,1]"
+        lm_skip_layer = parse_layer_type(lm_skip_layer, 28)
+
+
+        model = ShowUIForConditionalGeneration.from_pretrained(
+            model_path,
+            torch_dtype=torch.bfloat16,
+            device_map=device,
+            lm_skip_ratio=uimask_ratio, lm_skip_layer=lm_skip_layer,
+            attn_implementation="flash_attention_2",
+        )
+
+        ratios = [0, 0.2, 0.4, 0.6, 0.8, 1]
+        for ratio in ratios:
+            processor = ShowUIProcessor.from_pretrained(
+                model_path, 
+                min_pixels=min_pixels, max_pixels=max_pixels,
+                uigraph_train=uigraph_train, uigraph_test=uigraph_test, uigraph_diff=uigraph_diff, uigraph_rand=uigraph_rand,
+                uimask_pre=True, uimask_ratio=ratio, uimask_rand=uimask_rand,
+            )
+            text = processor.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+            image_inputs, video_inputs = process_vision_info(messages)
+            inputs = processor(
+                text=[text],
+                images=image_inputs,
+                videos=video_inputs,
+                padding=True,
+                return_tensors="pt",
+            )
+            inputs = inputs.to(device)
+            visual_tokens = inputs['pixel_values'].shape[0] // 4           
+            dropped_visual_tokens = inputs['input_ids'].shape[1] - inputs['select_mask'].sum()
+            print("Number of Visual Tokens :", visual_tokens - dropped_visual_tokens)
+            # Timed inference
+            start_event = torch.cuda.Event(enable_timing=True)
+            end_event = torch.cuda.Event(enable_timing=True)
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            times = []
+            tps_all = []
+            for i in range(5):
+                start_event.record()
+                with torch.no_grad():
+                    generated_ids = model.generate(**inputs, max_new_tokens=1)
+                end_event.record()
+                elapsed_time = start_event.elapsed_time(end_event)
+                # print(f"\nElapsed Time {i+1} : {elapsed_time:.2f} ms")
+                times.append(elapsed_time)
+
+                num_generated_tokens = generated_ids.shape[1] - inputs["input_ids"].shape[1]
+                
+                tps = num_generated_tokens * 1000 / elapsed_time
+                tps_all.append(tps)
+                print(
+                    f"Generated_tokens_num : {num_generated_tokens}, TPS : {tps}"
+                )
+                
+            torch.cuda.synchronize()
+            print(f"Average Elapsed Time : ", sum(times) / len(times))
 
     else:
         from transformers import Qwen2VLForConditionalGeneration, Qwen2VLProcessor
